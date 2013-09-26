b083a7bf99b47e5b6edd1356b38f0e1f3d09190d
aszkid <polbox360@hotmail.com>
2013-09-26 20:45:12 +0200

    Added simple font rendering + OpenSans

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,41 @@
 #include <SFML/Graphics.hpp>
 
+const int SW = 800;
+const int SH = 600;
+const sf::Color QWHITE = sf::Color(200, 200, 200);
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(SW, SH), "SFML works!");
+	
+	sf::Vertex line[] = {
+		sf::Vertex(sf::Vector2f(SW/2, 0), QWHITE),
+		sf::Vertex(sf::Vector2f(SW/2, SH), QWHITE),
+	};
+	
+	sf::Font font;
+	font.loadFromFile("fonts/opensans/OpenSans-Regular.ttf");
+	sf::Text text;
+	text.setFont(font);
+	text.setString("JS Bach - French Suite No. 2, I. Allemande");
+	text.setCharacterSize(20);
+	text.setColor(QWHITE);
+	text.setPosition(10, 10);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
+		window.clear(sf::Color(25, 25, 25));
+		window.draw(line, 2, sf::Lines);
+		window.draw(text);
+		window.display();
+	}
 
-    return 0;
+	return 0;
 }
